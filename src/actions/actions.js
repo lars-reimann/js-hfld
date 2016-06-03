0aef712a1b015164b8b4667997881adc5b454b93
Lars Reimann <mail@lars-reimann.eu>
2016-06-03 12:05:41 +0200

    Removed obsolete files

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -31,53 +31,6 @@ export function setTableSorting(sorting) {
     dispatcher.dispatch({ type: "SET_TABLE_SORTING", sorting});
 }
 
-/**
- * An alert message to display.
- */
-class Alert {
-
-    /**
-     * @param {String} type
-     * The type of the alert. This is one of "info", "success", "warning" or
-     * "error".
-     *
-     * @param {String} message
-     * The message to display.
-     */
-    constructor(type, message) {
-
-        /**
-         * The type of the alert.
-         *
-         * @type {String}
-         */
-        this.type = type;
-
-        /**
-         * The message to display.
-         *
-         * @type {String}
-         */
-        this.message = message;
-
-        /**
-         * The ID of the alert.
-         *
-         * @type {String}
-         */
-        this.id = Alert.idGenerator.next();
-    }
-}
-Alert.idGenerator = new IDGenerator("a");
-
-export function enqueueAlert(type, message) {
-    dispatcher.dispatch({ type: "ENQUEUE_ALERT", alert: new Alert(type, message) });
-}
-
-export function dequeueAlert() {
-    dispatcher.dispatch({ type: "DEQUEUE_ALERT" });
-}
-
 // Dialog ---------------------------------------------------------------------
 
 /**

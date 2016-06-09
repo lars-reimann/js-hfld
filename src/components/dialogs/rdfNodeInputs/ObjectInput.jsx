d283914eca672c74c743fea4aa57ee58286696e0
Lars Reimann <mail@lars-reimann.eu>
2016-06-09 12:50:30 +0200

    Changing the node interface of the object works

--- src/components/dialogs/rdfNodeInputs/ObjectInput.jsx
+++ src/components/dialogs/rdfNodeInputs/ObjectInput.jsx
@@ -1,38 +1,49 @@
 import React                                                 from "react";
 import {Modal, FormGroup, ControlLabel, FormControl, Button} from "react-bootstrap";
 
+import * as rdf from "@ignavia/rdf";
+
 import BlankNodeInput from "./BlankNodeInput.jsx";
 import LiteralInput   from "./LiteralInput.jsx";
 import NamedNodeInput from "./NamedNodeInput.jsx";
 
-function nodeInput(node) {
+const defaultBlankNode = new rdf.BlankNode("");
+const defaultLiteral   = new rdf.Literal("");
+const defaultNamedNode = new rdf.NamedNode("");
+
+function nodeInput(node, valid, handleChange) {
+    const props = {node, valid, handleChange};
     switch (node.interfaceName) {
     case "BlankNode":
-        return (
-            <BlankNodeInput
-                node={node}
-            />
-        );
+        return <BlankNodeInput {...props} />;
     case "Literal":
-        return (
-            <LiteralInput
-                node={node}
-            />
-        );
+        return <LiteralInput {...props} />;
     case "NamedNode":
-        return (
-            <NamedNodeInput
-                node={node}
-            />
-        );
+        return <NamedNodeInput {...props} />;
     }
 }
 
-function handleInterfaceNameChange(interfaceName) {
-    this.setState({ interfaceName });
+function handleInterfaceChange(interfaceName, handleChange) {
+    switch (interfaceName) {
+    case "BlankNode":
+        return handleChange({
+            node:  defaultBlankNode,
+            valid: false,
+        });
+    case "Literal":
+        return handleChange({
+            node:  defaultLiteral,
+            valid: false,
+        });
+    case "NamedNode":
+        return handleChange({
+            node:  defaultNamedNode,
+            valid: false,
+        });
+    }
 }
 
-export default function ({node, valid}) {
+export default function ({node, valid, handleChange}) {
     const interfaceName = node.interfaceName;
     return (
         <div>
@@ -41,13 +52,13 @@ export default function ({node, valid}) {
                 <FormControl
                     componentClass="select"
                     value={interfaceName}
-                    onChange={value => this.handleInterfaceNameChange(value)}>
+                    onChange={e => handleInterfaceChange(e.target.value, handleChange)}>
                     <option value="BlankNode">Blank Node</option>
                     <option value="Literal">Literal</option>
                     <option value="NamedNode">Named Node</option>
                 </FormControl>
             </FormGroup>
-            {nodeInput(node)}
+            {nodeInput(node, valid, handleChange)}
         </div>
     );
 }

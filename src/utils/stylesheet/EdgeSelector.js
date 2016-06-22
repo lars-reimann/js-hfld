35eddc772878ee593eceb2b6930f897acf0d5ade
Lars Reimann <mail@lars-reimann.eu>
2016-06-22 10:45:46 +0200

    Documented edge selectors

--- src/utils/stylesheet/EdgeSelector.js
+++ src/utils/stylesheet/EdgeSelector.js
@@ -1,7 +1,19 @@
 import * as rdf from "@ignavia/rdf";
 
+/**
+ * Selects edges in a graph.
+ */
 export default class EdgeSelector {
 
+    /**
+     * A factory method to create the appropriate subclass of EdgeSelector.
+     *
+     * @param {string} s
+     * The stringified selector.
+     *
+     * @return {EdgeSelector}
+     * The created selector.
+     */
     static makeSelector(s) {
         const anyRegex    = /^\*$/;
         const filterRegex = /^(\S*)\s*->\s*(\S*)\s*->\s*(\S*)$/;
@@ -19,22 +31,47 @@ export default class EdgeSelector {
         }
     }
 
+    /**
+     *
+     */
     constructor() {}
 
+    /**
+     * Returns a set of the affected edges.
+     *
+     * @param {Graph} rdfGraph
+     * The graph to filter.
+     *
+     * @return {Set}
+     * The IDs of the affected edges.
+     */
     getAffectedEdges(rdfGraph) {
         throw new Error("Calling an abstract method.");
     }
 
+    /**
+     * Tests if the given edge is affected by this selector.
+     */
     isAffectedEdge(rdfGraph, edgeId) {
         return this.getAffectedEdges(rdfGraph).has(edgeId);
     }
 }
 
+/**
+ * Selects all edges in the graph.
+ */
 class AnyEdgeSelector extends EdgeSelector {
+
+    /**
+     *
+     */
     constructor() {
         super();
     }
 
+    /**
+     * @override
+     */
     getAffectedEdges(rdfGraph) {
         const result = new Set();
         for (let edge of rdfGraph) {
@@ -42,9 +79,32 @@ class AnyEdgeSelector extends EdgeSelector {
         }
         return result;
     }
+
+    /**
+     * Stringifies the selector.
+     *
+     * @return {string}
+     * The stringified selector.
+     */
+    toString() {
+        return "*";
+    }
 }
 
+/**
+ * Filters the edges according to the subject, predicate and object properties.
+ */
 class FilterEdgesSelector extends EdgeSelector {
+
+    /**
+     * Parses the stringified node filter.
+     *
+     * @param {string} s
+     * The stringified node filter.
+     *
+     * @return {null|string|RDFNode}
+     * The created filter.
+     */
     static makeNodeFilter(s) {
         const anyRegex        = /^\*$/;
         const valueRegex      = /^"(.*)"$/;
@@ -52,7 +112,7 @@ class FilterEdgesSelector extends EdgeSelector {
         const exactNamedRegex = /^<(.*)>$/;
 
         if (anyRegex.test(s)) {
-            return undefined;
+            return null;
         } else if (valueRegex.test(s)) {
             const [, value] = valueRegex.exec(s);
             return value;
@@ -67,16 +127,47 @@ class FilterEdgesSelector extends EdgeSelector {
         }
     }
 
+    /**
+     * @param {null|string|RDFNode} subjectFilter
+     * The filter to apply to the subject property.
+     *
+     * @param {null|string|RDFNode} predicateFilter
+     * The filter to apply to the predicate property.
+     *
+     * @param {null|string|RDFNode} objectFilter
+     * The filter to apply to the object property.
+     */
     constructor(subjectFilter, predicateFilter, objectFilter) {
         super();
 
+        /**
+         * The filter to apply to the subject property.
+         *
+         * @type {null|string|RDFNode}
+         * @private
+         */
         this.subjectFilter = subjectFilter;
 
+        /**
+         * The filter to apply to the predicate property.
+         *
+         * @type {null|string|RDFNode}
+         * @private
+         */
         this.predicateFilter = predicateFilter;
 
+        /**
+         * The filter to apply to the object property.
+         *
+         * @type {null|string|RDFNode}
+         * @private
+         */
         this.objectFilter = objectFilter;
     }
 
+    /**
+     * @override
+     */
     getAffectedEdges(rdfGraph) {
         const result = new Set();
 
